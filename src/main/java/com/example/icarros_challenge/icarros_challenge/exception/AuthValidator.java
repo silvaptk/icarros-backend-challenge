d4c86f7ddb5a43cda32ddb3f2d75def6b227fe8d
Patrick Silva <patrick.silva2717@outlook.com>
2025-03-19 12:30:31 -0300

    feat: update the auth validator to throw all possible validation exceptions
    Prior to this commit, the validation exceptions were being
    thrown one at a time. For now on, they will be thrown as a
    collection

--- src/main/java/com/example/icarros_challenge/icarros_challenge/exception/AuthValidator.java
+++ src/main/java/com/example/icarros_challenge/icarros_challenge/exception/AuthValidator.java
@@ -2,6 +2,7 @@ package com.example.icarros_challenge.icarros_challenge.exception;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,14 +21,29 @@ public class AuthValidator {
      * @return `true` indicating that the password is valid
      */
     public Boolean validatePassword(String password) {
-        this.validatePasswordClass(password);
-        this.validatePasswordLength(password);
-        this.validatePasswordLowercaseLetters(password);
-        this.validatePasswordUppercaseLetters(password);
-        this.validatePasswordNumbers(password);
-        this.validatePasswordSpecialCharacters(password);
-        this.validatePasswordRepeatedCharacters(password);
-        this.validatePasswordUnexpectedCharacters(password);
+        List<Supplier<Void>> validations = List.of(
+                () -> this.validatePasswordClass(password),
+                () -> this.validatePasswordLength(password),
+                () -> this.validatePasswordLowercaseLetters(password),
+                () -> this.validatePasswordUppercaseLetters(password),
+                () -> this.validatePasswordNumbers(password),
+                () -> this.validatePasswordSpecialCharacters(password),
+                () -> this.validatePasswordRepeatedCharacters(password),
+                () -> this.validatePasswordUnexpectedCharacters(password)
+        );
+
+        List<ValidationException> exceptions = new ArrayList<>();
+        for (Supplier<Void> validation : validations) {
+            try {
+                validation.get();
+            } catch (ValidationException exception) {
+                exceptions.add(exception);
+            } catch (Exception exception) {}
+        }
+
+        if (!exceptions.isEmpty()) {
+            throw new ValidationsException(exceptions);
+        }
 
         return true;
     }
@@ -37,10 +53,12 @@ public class AuthValidator {
      * client may have sent no password at all
      * @param password
      */
-    private void validatePasswordClass(String password) {
+    private Void validatePasswordClass(String password) {
         if (password == null || password.getClass() != String.class) {
             throw new PasswordIsMissingException();
         }
+
+        return null;
     }
 
     /**
@@ -48,10 +66,12 @@ public class AuthValidator {
      * short
      * @param password
      */
-    private void validatePasswordLength(String password) {
+    private Void validatePasswordLength(String password) {
         if (password.length() < MIN_PASSWORD_LENGTH) {
             throw new PasswordTooShortException(MIN_PASSWORD_LENGTH);
         }
+
+        return null;
     }
 
     /**
@@ -59,11 +79,13 @@ public class AuthValidator {
      * not
      * @param password
      */
-    private void validatePasswordLowercaseLetters(String password) {
+    private Void validatePasswordLowercaseLetters(String password) {
         Pattern pattern = Pattern.compile("[a-z]");
         if (!pattern.matcher(password).find()) {
             throw new PasswordWithoutLowercaseLettersException();
         }
+
+        return null;
     }
 
     /**
@@ -71,51 +93,59 @@ public class AuthValidator {
      * not
      * @param password
      */
-    private void validatePasswordUppercaseLetters(String password) {
+    private Void validatePasswordUppercaseLetters(String password) {
         Pattern uppercaseLettersPattern = Pattern.compile("[A-Z]");
         if (!uppercaseLettersPattern.matcher(password).find()) {
             throw new PasswordWithoutUppercaseLettersException();
         }
+
+        return null;
     }
 
     /**
      * Validates if the password has numbers, throwing if not
      * @param password
      */
-    private void validatePasswordNumbers(String password) {
+    private Void validatePasswordNumbers(String password) {
         Pattern numbersPattern = Pattern.compile("\\d");
         if (!numbersPattern.matcher(password).find()) {
             throw new PasswordWithoutNumbersException();
         }
+
+        return null;
     }
 
     /**
      * Validates if the password has special characters, throwing if not
      * @param password
      */
-    private void validatePasswordSpecialCharacters(String password) {
+    private Void validatePasswordSpecialCharacters(String password) {
         Pattern specialCharactersPattern = Pattern.compile("[" + SPECIAL_CHARACTERS + "]");
         if (!specialCharactersPattern.matcher(password).find()) {
             throw new PasswordWithoutSpecialCharactersException();
         }
+
+        return null;
     }
 
     /**
      * Validates if the password has repeated characters, throwing if so
      * @param password
      */
-    private void validatePasswordRepeatedCharacters(String password) {
+    private Void validatePasswordRepeatedCharacters(String password) {
         Pattern repeatedCharactersPattern = Pattern.compile("(.).*\\1");
         if (repeatedCharactersPattern.matcher(password).find()) {
             throw new PasswordWithRepeatedCharactersException();
         }
+
+        return null;
     }
 
     /**
      * Validates if the password has unexpected characters, throwing if so
      * @param password
      */
-    private void validatePasswordUnexpectedCharacters(String password) {
+    private Void validatePasswordUnexpectedCharacters(String password) {
         Matcher unexpectedCharacterMatcher = Pattern.compile("[^a-zA-Z0-9" + SPECIAL_CHARACTERS + "]").matcher(password);
 
         List<String> unexpectedCharacters = new ArrayList<>();
@@ -127,6 +157,8 @@ public class AuthValidator {
         if (!unexpectedCharacters.isEmpty()) {
             throw new PasswordWithUnexpectedCharacterException(String.join(",", unexpectedCharacters));
         }
+
+        return null;
     }
-    
+
 }
