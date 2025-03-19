2f33cf23e2de947b4e8934d4e84ed1544899ec33
Patrick Silva <patrick.silva2717@outlook.com>
2025-03-19 10:51:18 -0300

    fix: update global exception handler to deal with no body request

--- src/main/java/com/example/icarros_challenge/icarros_challenge/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/example/icarros_challenge/icarros_challenge/controller/ExceptionControllerAdvice.java
@@ -3,11 +3,13 @@ package com.example.icarros_challenge.icarros_challenge.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.icarros_challenge.icarros_challenge.application.LogService;
 import com.example.icarros_challenge.icarros_challenge.dto.ErrorResponse;
+import com.example.icarros_challenge.icarros_challenge.exception.InvalidBodyException;
 import com.example.icarros_challenge.icarros_challenge.exception.ValidationException;
 
 /**
@@ -31,6 +33,15 @@ public class ExceptionControllerAdvice {
      */
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(ValidationException exception) {
+        this.logService.handleMessage(exception.toString());
+
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), exception.getCode()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidBodyException(HttpMessageNotReadableException providedException) {
+        InvalidBodyException exception = new InvalidBodyException();
+
         this.logService.handleMessage(exception.toString());
 
         return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), exception.getCode()), HttpStatus.BAD_REQUEST);
